b69bff315cc3eea6353cfd7ad132101b71dade8c
Rinku Sharma <rsharma3@xavient.com>
2016-04-22 15:28:17 +0530

    ApplicationConfiguration class updated

--- src/main/java/com/interview/configuration/ApplicationConfiguration.java
+++ src/main/java/com/interview/configuration/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -34,5 +35,10 @@ public class ApplicationConfiguration {
 		dataSource.setPassword(password);
 		return dataSource;
 
+	}
+	
+	@Bean(name="jdbcTemplate")
+	public JdbcTemplate getJdbcTemplate() {
+		return new JdbcTemplate(getDataSource()); 
 	}
 }
