34bfb8e45a3b8d78519ed00e08b622d867d695a1
Rinku Sharma <rsharma3@xavient.com>
2016-05-03 09:59:37 +0530

    Topic service validator

--- src/main/java/com/interview/rest/controller/TopicServiceController.java
+++ src/main/java/com/interview/rest/controller/TopicServiceController.java
@@ -1,11 +1,13 @@
 package com.interview.rest.controller;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,51 +16,83 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.interview.mysql.TopicService;
 import com.interview.pojo.Topic;
-import com.interview.validator.TopicValidator;
 
 @RestController
 @RequestMapping(value = "/interviewservice/topicservice/")
 public class TopicServiceController {
 
-	@Autowired
-	private TopicValidator topicValidator;
+	public static final String INVALID_TOPIC = "Topic Name can not be Blank";
+	public static final String TOPIC_SAVING_ERROR = "Sorry , Topic is not saved ";
+
 	@Autowired
 	private TopicService mysqlDbService;
 
-	@RequestMapping(value = "/addTopic", method = RequestMethod.PUT)
-	public ResponseEntity<String> addTopics(@RequestBody Topic topic) throws SQLException {
-		String response = null;
-		response = topicValidator.topicValidNew(topic);
-		if (response == null) {
-			response = mysqlDbService.addTopics(topic);
+	@RequestMapping(value = "/addTopic", method = RequestMethod.POST)
+	public ResponseEntity<String> addTopics(@RequestBody Topic topic) {
+		String message = null;
+		try {
+			if (StringUtils.isEmpty(topic)
+					&& StringUtils.isEmpty(topic.getTopic_name())) {
+				message = mysqlDbService.addTopics(topic);
+			} else {
+				message = INVALID_TOPIC;
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+			message = TOPIC_SAVING_ERROR;
 		}
-		return new ResponseEntity<String>(response,HttpStatus.OK);
+		return new ResponseEntity<String>(message, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/getTopic", method = RequestMethod.GET)
-	public ResponseEntity<List<Topic>> getTopics() throws SQLException {
-		List<Topic> listTopic = mysqlDbService.getTopic();
-		return new ResponseEntity<List<Topic>>(listTopic,HttpStatus.OK);
+	public ResponseEntity<List<Topic>> getTopics() {
+		List<Topic> topicList = null;
+		try {
+			topicList = mysqlDbService.getTopic();
+
+			if (topicList == null)
+				topicList = new ArrayList<Topic>();
+
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+			topicList = new ArrayList<Topic>();
+		}
+		return new ResponseEntity<List<Topic>>(topicList, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/getTopicByTopicName", method = RequestMethod.PUT)
-	public ResponseEntity<Topic> getTopicByName(@RequestParam String topicName) throws SQLException {
-		String topic_name = topicValidator.getTopicsByName(topicName);
-		if (topic_name != null) {
-			Topic topics = mysqlDbService.getTopicByName(topic_name);
-			return new ResponseEntity<Topic>(topics,HttpStatus.OK);
+	public ResponseEntity<Topic> getTopicByName(@RequestParam String topicName) {
+		Topic topic = null;
+		try {
+			if (!StringUtils.isEmpty(topicName)) {
+				topic = mysqlDbService.getTopicByName(topicName.toLowerCase());
+			} else {
+				topic = new Topic();
+			}
+
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+			topic = new Topic();
 		}
-		return null;
+		return new ResponseEntity<Topic>(topic, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/modifyTopicByTopicName", method = RequestMethod.PUT)
-	public ResponseEntity<Topic> modifyByTopicName(@RequestParam String oldTopicName, @RequestParam String newTopicName) throws SQLException {
-		String oldName = topicValidator.getTopicsByName(oldTopicName);
-		String newName = topicValidator.getTopicsByName(newTopicName);
-		if (oldName != null && newName != null) {
-			Topic topics = mysqlDbService.modifyByTopicName(oldName, newName);
-			return new ResponseEntity<Topic>(topics,HttpStatus.OK);
+	public ResponseEntity<Topic> modifyByTopicName(
+			@RequestParam String oldTopicName, @RequestParam String newTopicName) {
+		Topic topic = null;
+		try {
+			if (StringUtils.isEmpty(oldTopicName)
+					&& StringUtils.isEmpty(newTopicName)) {
+				topic = mysqlDbService.modifyByTopicName(oldTopicName,
+						newTopicName);
+			} else {
+				topic = new Topic();
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+			topic = new Topic();
 		}
-		return null;
+		return new ResponseEntity<Topic>(topic, HttpStatus.OK);
 	}
 }
